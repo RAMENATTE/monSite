b1f6901ca08cefc3c2dc2ae0d2491a2de355536d
david <ramenattedavid@hotmail.fr>
2017-02-07 09:52:33 +0100

    premier push

---
+++ acceuil.php
@@ -0,0 +1,21 @@
+<?php
+    include "includes/head.php";
+?>
+            
+        <header>
+            
+            <div id="header" class="container">
+                <div class="row">
+                    <div id="photo" class="col s4">
+                        
+                    </div>
+                </div>
+            </div>
+        
+        </header>
+        
+
+        <script src="js/materialize.min.js"></script>
+    </body>
+
+</html>
