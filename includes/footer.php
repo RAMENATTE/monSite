a37047acaf82caec25ef313e34f048f7524b1b78
david <ramenattedavid@hotmail.fr>
2017-02-07 16:17:38 +0100

    premiere journée

---
+++ includes/footer.php
@@ -0,0 +1,42 @@
+<footer class="page-footer teal grey lighten-1">
+	<div class="container">
+		<div class="row">
+			<div id="cv" class="col s4">
+				<h5 class="white-text">CV en ligne</h5>
+				<p>Dévéloppeur web (HTML, CSS, JS)</p>
+				<p>Intégrateur web (PHP, MySQL)</p>
+				<p>Dévéloppeur WordPress</p>
+			</div>
+			
+			<div id="coordonne" class="col s4">
+				<h5 class="white-text">Mes coordonnées</h5>
+				<p>Ramenatte David</p>
+				<p>Le bourg</p>
+				<p>69640 Montmelas Saint Sorlin</p>
+				<p>Email: ramenatted@gmail.com</p>
+				<p>Mobile: 00-00-00-00-00</p>
+				<p>Fixe: 00-00-00-00-00</p>
+			</div>
+			
+			<div id="lien" class="col s4">
+				<h5 class="white-text">Mes liens</h5>
+				<ul>
+					<li><a class="blue-text text-blue" href="#!">Facebook</a></li>
+					<li><a class="red-text text-red" href="#!">Gmail</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="footer-copyright">
+		<div class="container">
+			© 2017 Copyright Text
+			<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+		</div>
+	</div>
+</footer>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="js/materialize.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+
+</html>
--- includes/head.php
+++ includes/head.php
@@ -7,25 +7,10 @@
         <meta charset="utf-8">	
         <title>http://ramenatte.fr</title>
         <link href="https://fonts.googleapis.com/css?family=Pattaya" rel="stylesheet">
+		<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/materialize.min.css">
 
     </head>
 
-    <body>
-        
-        <!--MISE EN PLACE DU FOND-->
-            <canvas id="q" style="position: fixed;">
-                <script src="js/script.js"></script>
-            </canvas>
-        
-        <!--CODAGE DE LA NAVBAR-->
-
-            <nav>
-                <div class="nav-wrapper teal grey lighten-1">
-                    <ul id="nav-mobile" class="left hide-on-med-and-down">
-                        <li><a id="acceuil" href="acceuil.php">Acceuil</a></li>
-                        <li><a id="blog" href="blog.php">Blog</a></li>
-                    </ul>
-                </div>
-            </nav>
+  
